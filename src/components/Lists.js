efa634d3be83de22c18979e128f4f5ddb7520d33
Saurav Thapa <thapasaurav1996@gmail.com>
2023-02-05 22:07:58 +0530

    removed console.log statements

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -5,12 +5,10 @@ import { ReactComponent as Increment } from "../images/chevron_up_small.svg";
 import { ReactComponent as Decrement } from "../images/chevron_down_small.svg";
 import { ReactComponent as Btn } from "../images/glyph_x.svg";
 
-
 const Lists = (props) => {
   const { data, setData } = props;
-  console.log(data);
   const removeItemHandler = (itemName) => {
-    setData((prevData => prevData.filter((item) => item.name !== itemName)));
+    setData((prevData) => prevData.filter((item) => item.name !== itemName));
   };
 
   const removeAllItemHandler = () => {
@@ -21,10 +19,13 @@ const Lists = (props) => {
     event.preventDefault();
     const newItem_Name = event.target.newItem.value?.trim();
     const qty = Number(event.target.newItem_Qty.value);
-    const doesItemExist = data?.findIndex(item => item.name?.toLowerCase() === newItem_Name.toLowerCase()) !== -1;
+    const doesItemExist =
+      data?.findIndex(
+        (item) => item.name?.toLowerCase() === newItem_Name.toLowerCase()
+      ) !== -1;
     if (doesItemExist) {
       const prevData = [...data];
-      const modifiedData = prevData.map(item => {
+      const modifiedData = prevData.map((item) => {
         if (item.name.toLowerCase() === newItem_Name.toLowerCase())
           item.qty = item.qty + qty;
         return item;
@@ -34,34 +35,37 @@ const Lists = (props) => {
       const newItem = { name: newItem_Name, qty: qty };
       setData((prevData) => [...prevData, newItem]);
     }
-    document.getElementsByTagName('form')?.[0].reset();
+    document.getElementsByTagName("form")?.[0].reset();
     // if new item is already present in the Inventory then we have to increase the Qty of that item
     // data.forEach((item, index) => {
     //   if (item.name.includes(newItem_Name)) {
     //   }
     // });
-
   };
   const increaseQtyHandler = (itemName) => {
-    setData((prevData) => prevData.map(item => {
-      if (item.name === itemName)
-        return {
-          ...item,
-          qty: item.qty + 1,
-        };
-      console.log(item);
-      return item;
-    }))
+    setData((prevData) =>
+      prevData.map((item) => {
+        if (item.name === itemName)
+          return {
+            ...item,
+            qty: item.qty + 1,
+          };
+        console.log(item);
+        return item;
+      })
+    );
   };
   const deccreaseQtyHandler = (itemName) => {
-    setData((prevData) => prevData.map(item => {
-      if (item.name === itemName && item.qty > 1)
-        return {
-          ...item,
-          qty: item.qty - 1,
-        };
-      return item;
-    }))
+    setData((prevData) =>
+      prevData.map((item) => {
+        if (item.name === itemName && item.qty > 1)
+          return {
+            ...item,
+            qty: item.qty - 1,
+          };
+        return item;
+      })
+    );
   };
   return (
     <div>
@@ -73,20 +77,37 @@ const Lists = (props) => {
               <form onSubmit={addItemHandler} id="form">
                 <div className={classes.formContainer}>
                   <div className={classes.itemSection}>
-                    <label htmlFor="newItem" className={classes.label}>Item Name*</label>
-                    <input type="text" id="newItem" className={classes.itemInput} />
+                    <label htmlFor="newItem" className={classes.label}>
+                      Item Name*
+                    </label>
+                    <input
+                      type="text"
+                      id="newItem"
+                      className={classes.itemInput}
+                    />
                   </div>
                   <div className={classes.quantitySection}>
-                    <label htmlFor="newItem_Qty" className={classes.label}>Quantity*</label>
+                    <label htmlFor="newItem_Qty" className={classes.label}>
+                      Quantity*
+                    </label>
                     <div className="d-flex">
-                      <input type="number" min={1} id="newItem_Qty" className={classes.quantityInput} />
-                      <button type="submit" className="btn btn-primary">Add</button>
+                      <input
+                        type="number"
+                        min={1}
+                        id="newItem_Qty"
+                        className={classes.quantityInput}
+                      />
+                      <button type="submit" className="btn btn-primary">
+                        Add
+                      </button>
                     </div>
                   </div>
                 </div>
               </form>
             </div>
-            <div className={classes.getStarted}>To get started, add 1 or more items</div>
+            <div className={classes.getStarted}>
+              To get started, add 1 or more items
+            </div>
             <div>
               <h6 className={classes.inventoryHeader}>Inventory List</h6>
 
@@ -107,22 +128,33 @@ const Lists = (props) => {
                             <Decrement
                               alt="down"
                               onClick={() => deccreaseQtyHandler(item.name)}
-                              className={`${classes.ctaBtn} ${item.qty === 1 ? classes.btnDisabled : ''}`}
+                              className={`${classes.ctaBtn} ${
+                                item.qty === 1 ? classes.btnDisabled : ""
+                              }`}
                             />
-
                           </div>
                         </div>
                       </td>
-                      <td><Btn
-                        className={classes.removeBtn}
-                        onClick={() => removeItemHandler(item.name)}
-                      /></td>
+                      <td>
+                        <Btn
+                          className={classes.removeBtn}
+                          onClick={() => removeItemHandler(item.name)}
+                        />
+                      </td>
                     </tr>
                   ))}
                 </tbody>
               </Table>
             </div>
-            {<button disabled={!data.length} onClick={removeAllItemHandler} className={`btn btn-light ${classes.clearAll}`}>Clear All</button>}
+            {
+              <button
+                disabled={!data.length}
+                onClick={removeAllItemHandler}
+                className={`btn btn-light ${classes.clearAll}`}
+              >
+                Clear All
+              </button>
+            }
           </CardBody>
         </Card>
       </div>
--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,14 @@
 export const getDataFromLocalStorage = () => {
   let data = [];
   try {
-    console.log(localStorage.getItem('everdoseData'));
-    data = JSON.parse(localStorage.getItem('everdoseData'));
-    console.log(data);
+    data = JSON.parse(localStorage.getItem("everdoseData"));
     if (data.length === 0) {
       return null;
     }
-  } catch (e) {
-
-  }
+  } catch (e) {}
   return data;
-}
+};
 
 export const setDataToLocalStorage = (data) => {
-  localStorage.setItem('everdoseData', JSON.stringify(data));
-}
+  localStorage.setItem("everdoseData", JSON.stringify(data));
+};
